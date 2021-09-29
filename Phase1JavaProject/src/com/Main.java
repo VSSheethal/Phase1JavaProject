20ef9ab3413ae28ce982ee841f42392df9348931
sheethal v s <sheethalv.s22@gmail.com>
2021-09-29 18:30:36 +0000

    changed First project

--- Phase1JavaProject/src/com/Main.java
+++ Phase1JavaProject/src/com/Main.java
@@ -1,17 +1,9 @@
 package com;
-//import com.FileHandleManipulations;
-//import com.Manipulation;
-//import com.Option;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//Manipulation.createMainFolderIfNotPresent("main");
-		
-	//	Option.Welcome("LockedMe", "Sheethal V S");
-		
-		//FileHandleManipulations.handleWelcomeScreenInput();
 		
 Manipulation.createMainFolderIfNotPresent("main");
 		
